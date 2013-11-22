a37f653ff04221a0ad33cb1e0ee06a5f9c9deab7
EsKey <seppel875@hotmail.com>
2013-11-22 11:19:56 +0100

    relativer Pfad für Hintergrundbild funktioniert jetzt

--- uLTRA/frontend/frames/MainFrame.java
+++ uLTRA/frontend/frames/MainFrame.java
@@ -5,12 +5,13 @@ package frames;
 
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.MediaTracker;
-import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 /**
  * @author Sebastian Kiepert
@@ -25,18 +26,36 @@ public class MainFrame extends JFrame{
 	private JDesktopPane desktop;
 	private MenuFrame menuFrame = new MenuFrame(this);
 	private GameFrame gameFrame = new GameFrame(this);
+	private Image img;
 	
 	public MainFrame(){
 		setTitle("uLTRA");
 		setSize(800,600);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		setContentPane(new BackGroundPane("../../Documents/images/light.jpg"));
 		setVisible(true);
 	}
 	
-	public void init(){
-		desktop = new JDesktopPane();
+	public void init(){		
+		File imgFile = new File("../uLTRA/Documents/images/light.jpg");
+		try {
+			img = ImageIO.read(imgFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		desktop = new JDesktopPane()
+		{
+	        /**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;			
+			Image scaledImg = img.getScaledInstance(800, 600, Image.SCALE_SMOOTH);
+			public void paintComponent(Graphics g) {
+	            super.paintComponent(g);
+	            g.drawImage(scaledImg,0,0,this.getWidth(),this.getHeight(),this);
+	        }
+		};
 		desktop.add(menuFrame);
 		desktop.add(gameFrame);
 		desktop.setVisible(true);
@@ -50,31 +69,4 @@ public class MainFrame extends JFrame{
 	public GameFrame getGameFrame(){
 		return gameFrame;
 	}
-	
-    class BackGroundPane extends JPanel {
-        /**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-		Image img = null;
- 
-        BackGroundPane(String imagefile) {
-            if (imagefile != null) {
-                MediaTracker mt = new MediaTracker(this);
-                img = Toolkit.getDefaultToolkit().getImage(imagefile);
-                mt.addImage(img, 0);
-                try {
-                    mt.waitForAll();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
- 
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            g.drawImage(img,0,0,this.getWidth(),this.getHeight(),this);
-        }
-    }
-
 }
