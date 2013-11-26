4fa59c12416fcb3b77dfeef2f080a4ac09779914
v095376 <v095376@DENW0011WL38L.de.top.com>
2013-11-26 16:14:21 +0100

    Rahmen des Fensters entfernt Javadoc-Kommentierungen eingefügt

--- uLTRA/frontend/frames/MainFrame.java
+++ uLTRA/frontend/frames/MainFrame.java
@@ -28,26 +28,41 @@ public class MainFrame extends JFrame{
 	private EditorFrame editorFrame = new EditorFrame(this);
 	private Image img;
 	
+	/**
+	 * erzeugt ein rahmenloses Fenster der Größe 800*600
+	 * @author Sebastian Kiepert
+	 * @version 2.0
+	 * @see frames.MainFrame#init()
+	 */
 	public MainFrame(){
-		setTitle("uLTRA");
 		setSize(800,600);
 		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setUndecorated(true);
 		setVisible(true);
 	}
 	
+	/**
+	 * initialisiert das Anwendungsfenster und fügt einen Desktop hinzu, auf dem weiterhin gearbeitet wird.
+	 * Es werden alle internen Fenster initialisiert und dem Desktop hinzugefügt. Zusätzlich wird ein
+	 * Hintergrundbild geladen
+	 * @author Sebastian Kiepert
+	 * @see frames.MenuFrame#MenuFrame(MainFrame)
+	 * @see frames.GameFrame#GameFrame(MainFrame)
+	 * @see frames.EditorFrame#EditorFrame(MainFrame)
+	 * 
+	 */
 	public void init(){		
 		File imgFile = new File("../uLTRA/Documents/images/light.jpg");
 		try {
 			img = ImageIO.read(imgFile);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		desktop = new JDesktopPane()
 		{
 	        /**
-			 * 
+	         * erzeugt das Hintergrundbild des Hauptfensters, das im Hauptmenü zu sehen ist
+			 *
 			 */
 			private static final long serialVersionUID = 1L;			
 			Image scaledImg = img.getScaledInstance(800, 600, Image.SCALE_SMOOTH);
@@ -63,14 +78,26 @@ public class MainFrame extends JFrame{
 		this.add(this.desktop);
 	}
 	
+	/**
+	 * De-/Aktiviert das Hauptmenü
+	 * @param visible
+	 */
 	public void setMenuVisibility(boolean visible){
 		menuFrame.setVisible(visible);
 	}
 	
+	/**
+	 * De-/Aktiviert das Spielfenster
+	 * @param visible
+	 */
 	public void setGameVisibility(boolean visible){
 		gameFrame.setVisible(visible);
 	}
 	
+	/**
+	 * De-/Aktiviert das Editorfenster
+	 * @param visible
+	 */
 	public void setEditorVisibility(boolean visible){
 		editorFrame.setVisible(visible);
 	}
