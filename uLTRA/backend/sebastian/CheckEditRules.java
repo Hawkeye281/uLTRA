6597b79d9e183ec102b5180760128de91db80924
EsKey <seppel875@hotmail.com>
2014-01-21 13:49:53 +0100

    NEU: Fehlerarten werden separat gezählt NEU: Anzahl der Lichtquellen wird gezählt NEU: Anzahl der leeren Felder wird gezählt NEU: Anzahl der Beams wird gezählt

--- uLTRA/backend/sebastian/CheckEditRules.java
+++ uLTRA/backend/sebastian/CheckEditRules.java
@@ -15,33 +15,89 @@ import gamegrid.Turn;
  */
 public class CheckEditRules {
 	
-	public static void check(EditorController editCont, GamePanel gamePanel){
-		int failures = 0;
+	public static void check(GamePanel gamePanel){
+		int failures = 0, cellFailures = 0, lightFailures = 0;
 		for (int x=0; x < EditorController.getGridWidth();x++){
 			for (int y=0; y < EditorController.getGridHeight(); y++){
-				Cell cell = editCont.getCell(x, y);
+				Cell cell = EditorController.getCell(x, y);
 				if (!cell.isEmpty()){
 					if (cell.isLightSource()){
 						LightSource lightSource = (LightSource) cell.getContent();
 						if (lightSource.getCapacity() == 0){
 							gamePanel.getTurnList().addTurn(new Turn("[" + (x+1) + " links; " + (y+1) + " runter]: Lichtquelle = 0"));
-							failures++;
+							lightFailures++;
 						}
 						
 					}
 				}
 				else{
 					gamePanel.getTurnList().addTurn(new Turn("[" + (x+1) + " links; " + (y+1) + " runter]: Leere Zelle"));
-					failures++;
+					cellFailures++;
 				}
 			}
 		}
+		failures = cellFailures + lightFailures;
 		if (failures>0){
 			gamePanel.getTurnList().addTurn(new Turn(failures + " Fehler gefunden"));
+			gamePanel.getTurnList().addTurn(new Turn("davon " + lightFailures + " leere Lichtquellen"));
+			gamePanel.getTurnList().addTurn(new Turn("und " + cellFailures + " leere Zellen"));
 		}
 		else{
 			gamePanel.getTurnList().addTurn(new Turn("Keine Fehler gefunden"));
 		}
+	}
+	
+	public static int lightSourceCount(){
+		int count = 0;
+		for (int x = 0; x < EditorController.getGridWidth(); x++){
+			for (int y = 0; y < EditorController.getGridHeight(); y++){
+				Cell cell = EditorController.getCell(x, y);
+				if (!cell.isEmpty())
+					if (cell.isLightSource())
+						count++;
+			}
+		}
+		return count;
+	}
+	
+	public static int lightCapacityCount(){
+		int count = 0;
+		for (int x = 0; x < EditorController.getGridWidth(); x++){
+			for (int y = 0; y < EditorController.getGridHeight(); y++){
+				Cell cell = EditorController.getCell(x, y);
+				if (!cell.isEmpty())
+					if (cell.isLightSource()){
+						LightSource lSource = (LightSource) cell.getContent();
+						count += lSource.getCapacity();
+					}
+			}
+		}
+		return count;
+	}
+	
+	public static int emptyCellCount(){
+		int count = 0;
+		for (int x = 0; x < EditorController.getGridWidth(); x++){
+			for (int y = 0; y < EditorController.getGridHeight(); y++){
+				Cell cell = EditorController.getCell(x, y);
+				if (cell.isEmpty())
+					count++;
+			}
+		}
+		return count;
+	}
+	
+	public static int beamCount(){
+		int count = 0;
+		for (int x = 0; x < EditorController.getGridWidth(); x++){
+			for (int y = 0; y < EditorController.getGridHeight(); y++){
+				Cell cell = EditorController.getCell(x, y);
+				if (!cell.isEmpty())
+					if (cell.isBeam())
+						count++;
+			}
+		}
+		return count;
 	}
 
 }
