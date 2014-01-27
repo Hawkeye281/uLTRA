ddd2e33153fe375a90654dccee2fa5ed8fdffce0
EsKey <seppel875@hotmail.com>
2014-01-27 13:31:58 +0100

    Beam kann jetzt mitgeteilt werden, zu welcher LightSource es gehört

--- uLTRA/backend/gamegrid/Beam.java
+++ uLTRA/backend/gamegrid/Beam.java
@@ -10,12 +10,19 @@ import java.io.Serializable;
 public class Beam implements CellContent, Serializable {
 	private static final long serialVersionUID = -7518155070908255527L;
 	private BeamDirections direction;
+	private LightSource lightSource;
 	private boolean beamEnd;
 	
 	public Beam(BeamDirections initialDirection) {
 		direction = initialDirection;
 		beamEnd = false;
 	} 
+	
+	public Beam (BeamDirections initialDirection, boolean pBeamEnd, LightSource remainingLightSource){
+		this.direction = initialDirection;
+		this.beamEnd = pBeamEnd;
+		this.lightSource = remainingLightSource;
+	}
 	
 	public Beam(BeamDirections initialDirection, boolean pBeamEnd) {
 		direction = initialDirection;
@@ -30,12 +37,24 @@ public class Beam implements CellContent, Serializable {
 		direction = newDirection;
 	}
 	
+	public void setBeamEnd(boolean pBeamEnd){
+		this.beamEnd = pBeamEnd;
+	}
+	
+	public void changeBeamEnd(){
+		this.beamEnd = (this.beamEnd)? false : true;
+	}
+	
 	public boolean isBeamEnd()
 	{
 		return beamEnd;
 	}
 	
+	public LightSource getRemLightSource(){
+		return this.lightSource;
+	}
+	
 	public Beam clone() {
-		return new Beam(this.direction, this.beamEnd);
+		return new Beam(this.direction, this.beamEnd, this.lightSource);
 	}
 }
